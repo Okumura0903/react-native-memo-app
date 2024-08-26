4603cf5004f0dc499c99a11a9ac7700c183ce1a3
Okumura0903 <info@muworks.net>
2024-08-26 21:38:52 +0900

    divide component

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+interface Props {
+  children: string;
+}
+
+const CircleButton = ({ children }: Props): React.JSX.Element => {
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    height: 64,
+    width: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 48,
+  },
+});
+export default CircleButton;
