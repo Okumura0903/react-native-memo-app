290b1556d360a3f4ac1ab49aa5c4e0e1f46524a4
Okumura0903 <info@muworks.net>
2024-09-06 22:31:19 +0900

    detail

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,24 +1,43 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
+import { db, auth } from '../../config';
+import { onSnapshot, doc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import CircleButton from '../../components/CircleButton';
 import Icon from '../../components/Icon';
+import { Memo } from '../../../types/memo';
 
 const handlePress = (): void => {
   router.push('memo/edit');
 };
 
 const detail = (): React.JSX.Element => {
+  const { id } = useLocalSearchParams();
+  const [memo, setMemo] = useState<Memo | null>(null);
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id));
+    const unsubscribe = onSnapshot(ref, (doc) => {
+      const { bodyText, updatedAt } = doc.data() as Memo;
+      setMemo({ id: doc.id, bodyText, updatedAt });
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.memoHeader}>
-        <Text style={styles.memoTitle}>買い物リスト</Text>
-        <Text style={styles.memoDate}>2023年10月1日 10:00</Text>
+        <Text style={styles.memoTitle} numberOfLines={1}>
+          {memo?.bodyText}
+        </Text>
+        <Text style={styles.memoDate}>
+          {memo?.updatedAt.toDate().toLocaleString('ja-JP')}
+        </Text>
       </View>
       <ScrollView style={styles.memoBody}>
-        <Text style={styles.memoBodyText}>
-          買い物リスト あああああああああああああああ
-          あああああああああああああああああああああああああああ
-        </Text>
+        <Text style={styles.memoBodyText}>{memo?.bodyText}</Text>
       </ScrollView>
       <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
         <Icon name="pencil" size={40} color="#fff" />
@@ -51,10 +70,10 @@ const styles = StyleSheet.create({
     lineHeight: 16,
   },
   memoBody: {
-    paddingVertical: 32,
     paddingHorizontal: 27,
   },
   memoBodyText: {
+    paddingVertical: 32,
     fontSize: 16,
     lineHeight: 24,
     color: '#000',
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { router, useNavigation } from 'expo-router';
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
 import MemoListItem from '../../components/MemoListItem';
 import CircleButton from '../../components/CircleButton';
 import LogOutButton from '../../components/LogOutButton';
 import Icon from '../../components/Icon';
+import { db, auth } from '../../config';
+import { type Memo } from '../../../types/memo';
 
 const handlePress = (): void => {
   router.push('memo/create');
 };
 
 const list = (): React.JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
@@ -19,14 +23,32 @@ const list = (): React.JSX.Element => {
       },
     });
   }, []);
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+    const q = query(ref, orderBy('updatedAt', 'desc'));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [];
+      snapshot.forEach((doc) => {
+        const { bodyText, updatedAt } = doc.data();
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt,
+        });
+      });
+      setMemos(remoteMemos);
+    });
+    return unsubscribe;
+  }, []);
   return (
     <View style={styles.container}>
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
+      <FlatList
+        data={memos}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
+      />
       <CircleButton onPress={handlePress}>
         <Icon name="plus" size={40} color="#fff" />
       </CircleButton>
